15cb9e90f4f191623fe1cc6a127ce78bca66e885
Charles Ferreira <charles.dev@live.com>
2019-05-04 10:49:45 -0300

    feat(#6): include a header element

---
+++ frontend/src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,13 @@
+import { MatToolbarModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatToolbarModule
+  ],
+  exports: [
+    MatToolbarModule
+  ]
+})
+export class AngularMaterialModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
