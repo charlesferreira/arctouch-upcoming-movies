9014eb8e43b550cce939eb3fe0a06d4dad2dcfd1
Charles Ferreira <charles.dev@live.com>
2019-05-05 15:04:24 -0300

    refactor(#6): group the views into a folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { AngularMaterialModule } from './angular-material/angular-material.module';
-import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SearchBoxComponent } from './search-box/search-box.component';
-import { HeroComponent } from './hero/hero.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularMaterialModule } from './angular-material.module';
+import { AppComponent } from './app.component';
+import { HeroComponent } from './views/hero/hero.component';
+import { SearchBoxComponent } from './views/search-box/search-box.component';
 
 @NgModule({
   declarations: [AppComponent, SearchBoxComponent, HeroComponent],
